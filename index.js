0bb838b0054eb1a4022d15b2e3e5d987685a00a2
Kenneth Bilicke <kbilicke7891@gmail.com>
2018-03-12 01:08:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- var price = math.floor(math.random()*100)
+ var price = Math.floor(Math.random()*100)
  var newItem = { item: price};
  
  return `${item} has been added to your cart.`
